1b0958a70bf3842531989dbf896c3bee54adf64d
Revillion Gabriel <gabriel_revillion@ens.univ-artois.fr>
2020-02-11 16:46:09 +0100

    [GIT] first commit

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** portal that display what it sees on the other portal and manage teleportation **/
+public class Portal : MonoBehaviour
+{
+
+    // the other portal
+    [SerializeField]
+    private Portal linkedPortal;
+
+    [SerializeField]
+    private MeshRenderer screen;
+
+    [SerializeField]
+    private Camera cam;
+
+    // texture for the other portal
+    private RenderTexture viewTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+        cam.targetTexture = viewTexture;
+        linkedPortal.screen.material.SetTexture("_MainTex", viewTexture);
+
+        //cam.enabled = false;
+    }
+
+    private void Update()
+    {
+        cam.transform.localPosition = Camera.main.transform.position- linkedPortal.transform.position;
+        cam.transform.rotation = Camera.main.transform.rotation;
+    }
+
+
+}
